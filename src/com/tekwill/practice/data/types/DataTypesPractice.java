d27e04e951d79119fd4a27bd6b7ee9c3bce9ea9a
aminoiu <minoiuanna@gmail.com>
2022-01-31 20:35:54 +0200

    First commit

---
+++ src/com/tekwill/practice/data/types/DataTypesPractice.java
@@ -0,0 +1,13 @@
+package com.tekwill.practice.data.types;
+
+public class DataTypesPractice {
+
+    public static void main(String[] args) {
+        long i=50564654654L;
+        int i3= (int) i;
+
+
+        System.out.println(i3);
+
+    }
+}
